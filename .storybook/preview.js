4f8bac42241e3ab7f17d16592b25ec94b8519be6
nicholasprussen <nicholasprussen@u.boisestate.edu>
2022-08-19 22:04:30 -0600

    Added storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,17 @@
+// .storybook/preview.js
+
+import '../src/tailwind.css';
+
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  previewTabs: {
+    'storybook/docs/panel': { index: -1 },
+  },
+};
